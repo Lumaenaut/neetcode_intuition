f4e6d0e92e01b26ff415276d9471d55bef1d66dd
PanCosmica <albertodvcano@outlook.com>
2025-07-05 18:53:58 -0400

    Added resize functionality to the program and now each value can be modified.

--- arrays_and_hashing/contains_duplicate/script.js
+++ arrays_and_hashing/contains_duplicate/script.js
@@ -1,37 +1,62 @@
 const baseList = [1, 2, 3, 5, 10, 5, 9, 8, 4, 7];
 const numListEl = document.getElementById('num-list');
-const sliderEl = document.getElementById('list-slider');
+const handle = document.getElementById('handle')
 
+const pixelsPerItem = 50; // mouse sensitivity when dragging, bigger is less sensitive
 let currentLength = 7; // initial list size
 let isDragging = false;
 let startX = 0;
 
 function renderList(length) {
+    numListEl.innerHTML = '';
     const displayList = baseList.slice(0, length);
-    numListEl.textContent = displayList.join(" , ");
+
+    displayList.forEach((value, index) => {
+        const input = document.createElement('input');
+        input.type = 'number';
+        input.value = value;
+        input.className = "list-input";
+        input.dataset.index = index;
+
+        input.addEventListener('input', (e) => {
+            const i = parseInt(e.target.dataset.index);
+            baseList[i] = parseInt(e.target.value);
+        });
+
+        numListEl.appendChild(input);
+
+        if (index <= displayList.length - 1) {
+            const comma = document.createElement('span');
+            comma.textContent = ' , ';
+            numListEl.appendChild(comma);
+        }
+    });
+
 }
 
 renderList(currentLength);
 
-// DRAG LOGIC
 handle.addEventListener("mousedown", (e) => {
     isDragging = true;
     startX = e.clientX;
-    document.body.style.cursor = "ew-resize";
+    document.body.style.cursor = "ew-resize"; // changing cursor shape
 });
 
 document.addEventListener("mousemove", (e) => {
+    
     if (!isDragging) return;
 
-    const dx = e.clientX - startX;
-    const pixelsPerItem = 30; // adjust sensitivity
-    const deltaItems = Math.round(dx / pixelsPerItem);
-    let newLength = currentLength + deltaItems;
-    newLength = Math.max(0, Math.min(baseList.length, currentLength + deltaItems));
+    const dx = e.clientX - startX; 
 
-    if (newLength !== currentLength) {
-        currentLength = newLength;
-        renderList(currentLength);
+    if (Math.abs(dx) >= pixelsPerItem) {
+        const direction = Math.sign(dx);
+        const newLength = currentLength + direction
+
+        if (newLength >= 0 && newLength <= baseList.length) {
+            currentLength = newLength;
+            renderList(currentLength);
+            startX += direction * pixelsPerItem;
+        }
     }
 });
 
